196f451e2a760a2378218bd63f8024d06b74786b
luk716 <60812868+luk716@users.noreply.github.com>
2020-02-14 15:51:40 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,35 +1,35 @@
 #include<iostream>
 #include<ctime>
 using namespace std;
-void Fillarr(int arr[3][8], int const ROW,int const WAL)
-{
-	for (int i = 0; i < ROW; i++)
-	{
-		for (int j = 0; j < WAL; j++)
-		{
-			arr[i][j] = rand() % 20;
-		}
-	}
-}
-void Printarr(int arr[3][8], int const ROW, int const WAL)
-{
-	for (int i = 0; i < ROW; i++)
-	{
-		for (int j = 0; j < WAL;j++)
-		{
-			cout << arr[i][j]<< "  ";
-		}
-		cout << endl;
-	}
-}
 void main()
 {
-	setlocale(LC_ALL, "Ru"); 
-	int ROW = 3,WAL=8;
-	srand(time(NULL));
-	int g=10;
-	cout<<g<<endl;
-	int arg[3][8];
-	Fillarr(arg, ROW, WAL);
-	Printarr(arg, ROW, WAL);
+	setlocale(LC_ALL, "ru");
+		srand(time(NULL));
+		int arr[15];
+		int Variable;
+		bool value;
+		for (int i = 0; i < 15;)
+		{
+			value = false;
+			Variable = rand()%40;
+			for (int j = 0; j < i+1; j++)
+			{ 
+				if (arr[j] == Variable) 
+				{
+					value = true;
+					break;
+				}
+				if(!value)
+				{
+					arr[i] = Variable;
+				
+				}
+			}
+			i++;
+		}
+		for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++)
+		{
+			cout << arr[i] << " ";
+		}
+
 }
