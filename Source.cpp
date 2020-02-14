9350bae025b32f9fe879e3cee36535b658ae511d
luk716 <60812868+luk716@users.noreply.github.com>
2020-02-14 15:55:37 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -4,32 +4,41 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "ru");
-		srand(time(NULL));
-		int arr[15];
+	srand(time(NULL));
+	const int SIZE = 20;
+	int arr[SIZE][SIZE];
 		int Variable;
-		bool value;
-		for (int i = 0; i < 15;)
+		bool  value;
+
+		for (int i = 0; i < SIZE; )
 		{
+			Variable = rand() % 80;
 			value = false;
-			Variable = rand()%40;
-			for (int j = 0; j < i+1; j++)
-			{ 
-				if (arr[j] == Variable) 
+
+			for (int j = 0; j < i; j++)
+			{
+				if (arr[j][j] == Variable)
 				{
 					value = true;
 					break;
 				}
-				if(!value)
-				{
-					arr[i] = Variable;
-				
-				}
+
 			}
-			i++;
+			if (!value)
+			{
+				arr[i][i] = Variable;
+				++i;
+			}
+			
 		}
-		for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++)
+		for (int i = 0; i < SIZE; i++)
 		{
-			cout << arr[i] << " ";
+			cout << arr[i][i] << endl;
 		}
+
+
+
+
+
 
 }
