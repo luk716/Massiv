3e92ae0a1ef36ccb223ea210d54a8fb8118f7f1b
luk716 <60812868+luk716@users.noreply.github.com>
2020-05-07 17:22:35 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,35 +1,98 @@
 #include<iostream>
 #include<ctime>
 using namespace std;
-void Fillarr(int arr[3][8], int const ROW,int const WAL)
+void FillArr(int aru[10][10],int const ROW = 10,int const RoW =10) 
 {
 	for (int i = 0; i < ROW; i++)
 	{
-		for (int j = 0; j < WAL; j++)
+		for (int j = 0; j < RoW; j++)
 		{
-			arr[i][j] = rand() % 20;
+			 aru[i][j] = rand() % 10;
 		}
 	}
+
 }
-void Printarr(int arr[3][8], int const ROW, int const WAL)
+void PrintArr(int aru[10][10], int const ROW = 10, int const RoW = 10)
 {
 	for (int i = 0; i < ROW; i++)
 	{
-		for (int j = 0; j < WAL;j++)
+		for (int j = 0; j < RoW; j++)
 		{
-			cout << arr[i][j]<< "  ";
+			cout << aru[i][j] << "    ";
 		}
 		cout << endl;
 	}
 }
 void main()
 {
-	setlocale(LC_ALL, "Ru"); 
-	int ROW = 3,WAL=8;
+	setlocale(LC_ALL, "ru");
 	srand(time(NULL));
-	int g=10;
-	cout<<g<<endl;
-	int arg[3][8];
-	Fillarr(arg, ROW, WAL);
-	Printarr(arg, ROW, WAL);
+	const int ROW = 10;
+	const int RoW = 10;
+
+	int arg[ROW][RoW];
+	FillArr(arg,ROW);
+	PrintArr(arg,ROW);
+	int min = 100;
+	bool value = false;
+	int pam = 0;
+	int k = 0;
+	int art[10];
+
+	int arr[ROW];
+	int variable;
+	for (int i = 0; i < ROW; i++)
+	{
+		pam = arg[i][i];
+
+		for (int j = 0; j < ROW; j++)
+		{
+
+				if (arg[i][j] < min)
+				{  
+					min = arg[i][j];
+					
+				
+				}
+				//variable = arg[i][j];
+				arg[i][i] = min;
+				art[i] = min;
+
+		}
+		arr[i] = pam;
+		min = 100;
+	}
+	int Min = 100;
+	int Valik = 0;
+	int ValiK = 0;
+	int ary[10];
+	for (int i = 0; i < ROW;i++)
+	{
+		for (int j = 0; j < ROW;j++)
+		{
+		   if(i != j)
+		   {
+			   
+				   if (arg[i][j] == art[i])
+				   {
+					   arg[i][j] = arr[i];
+					   break;
+				   }
+			   
+		   }
+		}
+	}
+	cout << "ÂÒÎÐÎÉ ÌÀÑÑÈÂ" << endl;
+
+	PrintArr(arg,ROW,RoW);
+	cout << "ÒÐÅÒÈÉ ÌÀÑÑÈÂ " << endl;
+	for (int i = 0; i < ROW; i++)
+	{
+		cout << arr[i] << endl;		
+
+
+	}
+	cout << endl;
+
+
 }
