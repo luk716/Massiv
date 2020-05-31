472673d90896ab2b0fb868f7e4bd1215a878c269
luk716 <60812868+luk716@users.noreply.github.com>
2020-05-31 13:14:56 +0300

    task_6

---
+++ A.java
@@ -0,0 +1,17 @@
+package task_6;
+
+public class A {
+    public A( float a,float b) {
+    }
+
+    public void A(float a, float b )
+    {
+        if(a == b)
+        {
+            System.out.println("Прямоугрольник является квадратом ");
+        }
+        else {
+            System.out.println("Прямоугольник не является квадратом ");
+        }
+    }
+}
---
+++ Programm_Six.java
@@ -0,0 +1,12 @@
+package task_6;
+
+import java.util.Scanner;
+
+public class Programm_Six {
+    public static void main(String[] args) {
+        Scanner src = new Scanner(System.in);
+        float a = src.nextFloat();
+        float b = src.nextFloat();
+        A kv = new A(a,b);
+        kv.A(a,b);
+}}
